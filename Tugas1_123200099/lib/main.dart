ebb8e1b3ad766684833ac18890bb25cb81c164b8
Panca AR <pancarahman313@gmail.com>
2023-03-05 17:48:02 +0700

    Tugas 1

---
+++ Tugas1_123200099/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '123200099 - Panca Aulia Rahman',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
